2a508fa47aad9ec9e7ca178a3314765a697fdfa2
Maria Cilloniz <majocilloniz@gmail.com>
2024-11-27 18:09:21 -0800

    Work on resposiveness

--- src/api/SubredditAnalyzerApi.js
+++ src/api/SubredditAnalyzerApi.js
@@ -4,12 +4,12 @@ const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const getSubredditAnalysis = async (subreddit) => {
     try {
-      const { data } = await axios.get(`${baseUrl}/api/reddit/${subreddit}`);
-      return data;
+        const { data } = await axios.get(`${baseUrl}/api/reddit/${subreddit}`);
+        return data;
     } catch (error) {
-      console.error("Could not fetch subreddit analysis:", error);
-      throw new Error("Error fetching subreddit analysis.");
+        console.error("Could not fetch subreddit analysis:", error);
+        throw new Error("Error fetching subreddit analysis.");
     }
-  };
-  
-  export { getSubredditAnalysis };
+};
+
+export { getSubredditAnalysis };
